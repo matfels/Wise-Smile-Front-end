defee80473401904948fb7e30ba9bc730a508b01
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-06-13 03:20:20 -0300

    Título do Commit:feat(login): implementa tela de autenticação e define paleta de cores globais
    Criação da interface de login e estruturação do dicionário visual do Wise Smile.
    
    Detalhes das implementações:
    - Interface: Construção do LoginComponent para receber as credenciais de acesso e preparação para futura integração com os Tokens JWT da API.
    - Padronização Visual: Configuração do dicionário de variáveis CSS globais no styles.scss, incorporando a identidade visual principal (azul marinho e dourado) e cores de sistema (sucesso/erro).
    - Refatoração: Higienização da estrutura de pastas com a remoção de arquivos .spec.ts isolados para manter o repositório focado apenas em código de produção.

--- wise-smile/src/app/app.routes.ts
+++ wise-smile/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Rota raiz (quando não tem nada na URL, carrega a Home)
+  { path: '', component: HomeComponent },
+  
+  // Rota de login
+  { path: 'login', component: LoginComponent }
+];
