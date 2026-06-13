f9943aa875b79586282f156a4a0c92ee0277849a
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-06-13 15:46:53 -0300

    feat: implementa captura de dados e eventos no formulário de login
    Estrutura os campos de e-mail e senha no template HTML da página de login.
    
    Implementa o envio do formulário associado ao método correspondente no componente TypeScript.
    
    Valida a captura e consistência das credenciais digitadas através de logs de depuração no console.
    
    Prepara o componente para a integração com o serviço de autenticação HTTP.

--- wise-smile/src/app/pages/login/login.component.ts
+++ wise-smile/src/app/pages/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; //Habilita a leitura de formulários 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent {
+
+
+  //Variaveis com o mesmo nome no arquivo DadosAutenticacao.java (Evitar erros na conver~sao para JSON)
+  credenciais = {
+    email: '',
+    senha: ''
+
+  };
+
+
+  fazerLogin() {
+    console.log('Dados prontos para enviar ao Java:', this.credenciais);
+  }
 
 }
