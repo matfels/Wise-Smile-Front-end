5f13e62ead4e525f7f8e1515561dfaf4384426e4
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-06-14 17:03:20 -0300

    feat: cria tela de início e implementa redirecionamento pós-login
    Desenvolve o componente da tela inicial (Dashboard/Home) do sistema Wise Smile.
    
    Configura as rotas do Angular (AppRoutes) para mapear a nova tela de início.
    
    Injeta o serviço Router no componente de login para realizar o redirecionamento automático após o sucesso da autenticação.
    
    Melhora a experiência do usuário estabelecendo o fluxo lógico de navegação principal.

--- wise-smile/src/app/app.routes.ts
+++ wise-smile/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-
+import { LoginComponent } from './pages/login/login.component'; // importa tela login
+import { HomeComponent } from './pages/home/home.component'; // importa tela home
+import { InicioComponent } from './pages/inicio/inicio.component'; // importa tela inicio 
 export const routes: Routes = [
   // Rota raiz (quando não tem nada na URL, carrega a Home)
   { path: '', component: HomeComponent },
   
   // Rota de login
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+
+  // rota inicio
+  { path: 'inicio', component: InicioComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' } // redireciona para o inivio caso não tenha o hrl correto.
 ];
--- wise-smile/src/app/pages/login/login.component.ts
+++ wise-smile/src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; //Habilita a leitura de formulários 
 import { AuthService } from '../../services/auth.service'; // Importa o arquivo auth.service.ts
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -19,15 +21,16 @@ export class LoginComponent {
 
   };
 // Chama o auth.service.ts quando a tela é carregada
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,private router: Router) {}
 
   fazerLogin() {
     //Chama o auth.service.ts e o (subscribe) para ficar à espera da resposta do Java
     this.authService.fazerLogin(this.credenciais).subscribe({
       next: (resposta) => {
         // Se a senha estiver certa, o Java devolve o código 200 e entra aqui
-        console.log('Login efetuado com sucesso! Token recebido:', resposta);
-        alert('Bem-vindo ao Wise Smile!');
+        console.log(' Login efetuado com sucesso! Token recebido:' , resposta);
+        localStorage.setItem('token', resposta.token);
+        this.router.navigate(['/inicio']);
       },
       error: (erro) => {
         // Se a senha estiver errada, o Java devolve código 400/403 e entra aqui
