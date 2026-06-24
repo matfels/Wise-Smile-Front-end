7b34bbca9262a0111774044e19cec65f1c4b7fcf
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-06-23 22:21:25 -0300

    feat: tornar os cards de pacientes e dentistas dinâmicos no dashboard
    - Integração do InicioComponent com PacienteService e DentistaService.
    - Criação do método carregarEstatisticas() para buscar as listas diretamente na API.
    - Implementação de filtros (ativo === true) no TypeScript para garantir a contagem exata e em tempo real.
    - Substituição dos valores estáticos no HTML pelo Data Binding das novas variáveis (totalPacientesAtivos e totalDentistasAtivos).
    - Otimização do dashboard para refletir o estado real da base de dados de forma automatizada.

--- wise-smile/src/app/pages/inicio/inicio.component.ts
+++ wise-smile/src/app/pages/inicio/inicio.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AgendamentoService } from '../../services/agendamento.service'; 
+import { PacienteService } from '../../services/paciente.service'; 
+import { DentistaService } from '../../services/dentista.service';
 
 @Component({
   selector: 'app-inicio',
@@ -16,14 +18,20 @@ export class InicioComponent implements OnInit {
   consultasDoDia: any[] = [];
   dataAtual = new Date(); 
   totalConsultasHoje: number = 0;
+  totalPacientesAtivos: number = 0;
+  totalDentistasAtivos: number = 0;
+
   constructor(
     private router: Router,
-    private agendamentoService: AgendamentoService 
+    private agendamentoService: AgendamentoService,
+    private pacienteService: PacienteService,
+    private dentistaService: DentistaService
   ) {}
 
   // Dispara automaticamente quando a página de Início carrega
   ngOnInit() {
     this.carregarAgendaDashboard();
+    this.carregarEstatisticas();
   }
 
 carregarAgendaDashboard() {
@@ -49,6 +57,26 @@ carregarAgendaDashboard() {
         this.totalConsultasHoje = apenasHoje.length;
       },
       error: (err) => console.error('Erro ao carregar a agenda no dashboard', err)
+    });
+  }
+
+  carregarEstatisticas() {
+    
+    this.pacienteService.listar().subscribe({
+      next: (pacientes) => {
+        const pacientesAtivos = pacientes.filter((p: any) => p.ativo === true);
+        this.totalPacientesAtivos = pacientesAtivos.length;
+      },
+      error: (err) => console.error('Erro ao buscar pacientes', err)
+    });
+
+
+    this.dentistaService.listar().subscribe({
+      next: (dentistas) => {
+        const dentistasAtivos = dentistas.filter((d: any) => d.ativo === true);
+        this.totalDentistasAtivos = dentistasAtivos.length;
+      },
+      error: (err) => console.error('Erro ao buscar dentistas', err)
     });
   }
 
